040a6edb62c68a62b5e03c04db0f3678bbe8871c
Per Egertun <per.egertun@chasacademy.se>
2020-09-24 13:11:56 +0200

    add api functionality

--- server/app.js
+++ server/app.js
@@ -1,83 +1,19 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const { graphqlHTTP } = require("express-graphql");
-const { buildSchema } = require("graphql");
 const mongoose = require("mongoose");
+
 require("dotenv").config();
 
-const Event = require("./models/event");
 const app = express();
 
-app.use(bodyParser.json());
+mongoose.connect(process.env.DB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-app.use(
-  "/graphql",
-  graphqlHTTP({
-    schema: buildSchema(`
-    type Event{
-        _id: ID!
-        title: String!
-        description: String!
-        price: Float!
-        date: String!
-    }
-
-    input EventInput {
-        title: String!
-        description: String!
-        price: Float!
-        date: String!
-
-    }
-    type RootQuery {
-        events: [Event!]!
-    }
-
-    type RootMutation{
-        createEvent(eventInput: EventInput): Event
-    }
-
-    schema {
-        query: RootQuery
-        mutation: RootMutation
-    }
-    `),
-    rootValue: {
-      events: () => {
-        return events;
-      },
-      createEvent: (args) => {
-        const event = new Event({
-          title: args.eventInput.title,
-          description: args.eventInput.description,
-          price: +args.eventInput.price,
-          date: new Date(args.eventInput.date),
-        });
-        return event
-          .save()
-          .then((result) => {
-            console.log(result);
-            return { ...result._doc };
-          })
-          .catch((err) => {
-            console.log(err);
-            throw err;
-          });
-      },
-    },
-    graphiql: true,
-  })
-);
-
-mongoose
-  .connect(process.env.DB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then((result) => {
-    console.log("connected to db");
-    app.listen(4000);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+const db = mongoose.connection;
+db.on("error", (error) => console.log(error));
+db.once("open", () => console.log("connected to db"));
+app.use(express.json());
+const subscribersRouter = require("./routes/subscribers");
+app.use("/subscribers", subscribersRouter);
+app.listen(3000, () => console.log("server started"));
